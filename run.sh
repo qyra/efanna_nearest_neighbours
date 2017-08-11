8151b6595295480096e061d8455567cdbbaec6ab
Karl <karl9242@gmail.com>
2017-08-10 19:44:21 -0500

    Standardizing compilation script names

--- run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -eu
-cd build
-make
-mv efanna ../
-cd ../
-time ./efanna
-#~ /usr/bin/time -v ./efanna
-#~ valgrind --tool=callgrind ./efanna # use kcachegrind to read the callgrind.out.x file.
